9e5af87079422b5fe758f113421726f7c9e8fb68
Ben Ebel <bge1234@gmail.com>
2016-06-15 21:07:59 -0700

    added create, update, and delete routes

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,52 @@ var knex = require('../db/knex');
 // - Auto-load from Strava using AJAX.
 
 router.get('/', function(req, res, next) {
+  res.redirect('/rides');
+});
+
+router.get('/rides/new', function(req, res, next) {
+  res.render('new', {});
+});
+
+router.post("/rides", function(req, res, next) {
+  knex('rides').insert({
+    date: req.body.date,
+    url: req.body.url
+  }).then(function(results) {
+    res.redirect("/rides");
+  });
+});
+
+router.get('/rides', function(req, res, next) {
   knex('rides').select().orderBy('id', 'desc').then(function(results) {
-      res.render('index', {rides: results});
+    res.render('index', {rides: results});
+  });
+});
+
+router.get('/rides/:id/edit', function(req, res, next) {
+  knex('rides').where("id", req.params.id).select().then(function(result) {
+    res.render('edit', { ride: result[0] });
+  });
+});
+
+router.post('/rides/:id', function(req, res, next) {
+  knex('rides').where("id", req.params.id).update({
+    date: req.body.date,
+    url: req.body.url
+  }).then(function(results) {
+    res.redirect('/rides');
+  });
+});
+
+router.get('/rides/:id/delete', function(req, res, next) {
+  knex('rides').where("id", req.params.id).select().then(function(result) {
+    res.render('delete', { ride: result[0] });
+  });
+});
+
+router.post('/rides/:id/delete', function(req, res, next) {
+  knex('rides').where("id", req.params.id).del().then(function(results) {
+    res.redirect('/rides');
   });
 });
 
--- seeds/seed_rides.js
+++ seeds/seed_rides.js
@@ -4,6 +4,6 @@ exports.seed = function(knex, Promise) {
     knex('rides').del(),
 
     // Inserts seed entries
-    knex('rides').insert({id: 1, date: 'May 16, 2016, 14:34', url: 'https://connect.garmin.com/modern/activity/1171715399'})
+    knex('rides').insert({id: 1, date: 'May 16, 2016', url: 'https://connect.garmin.com/modern/activity/1171715399'})
   );
 };
